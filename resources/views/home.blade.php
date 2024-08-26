8b7f90c89b5ecde5699eda246a7c54488fff8213
Shokhrukh <shohruhhon@gmail.com>
2024-08-26 21:37:58 +0500

    home page install

---
+++ resources/views/home.blade.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description"
+          content="Эксперт в области поисковой оптимизации, он помогает бизнесам улучшить видимость в интернете и привлечь больше клиентов."/>
+    <title>Шохрух Солеев — опытный SEO-специалист из Ташкента, Узбекистан. </title>
+    <link rel="canonical" href="https://soleyev.uz" />
+    <!-- Favicon -->
+    <link href="../assets/images/favicon.png" rel="shortcut icon">
+    <!-- CSS -->
+    <link href="../assets/plugins/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/plugins/magnific-popup/magnific-popup.min.css" rel="stylesheet">
+    <link href="../assets/plugins/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="../assets/plugins/owl-carousel/owl.theme.default.min.css" rel="stylesheet">
+    <link href="../assets/plugins/justified-gallery/justified-gallery.min.css" rel="stylesheet">
+    <link href="../assets/plugins/scrollcue/scrollcue.css" rel="stylesheet">
+    <link href="../assets/css/main.css" rel="stylesheet">
+    <!-- Fonts/Icons -->
+    <link href="../assets/plugins/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/plugins/font-awesome/css/all.css" rel="stylesheet">
+    <link href="../assets/plugins/themify/themify-icons.min.css" rel="stylesheet">
+</head>
+<body data-preloader="3">
+
+<!-- Animated Cursor -->
+<div id="animated-cursor"></div>
+<!-- end Animated Cursor -->
+
+<div class="wrapper">
+
+    <!-- Scroll to Top -->
+    <div class="scrolltotop">
+        <a class="button-circle button-circle-md button-circle-black" href="#"><i class="bi bi-arrow-up"></i></a>
+    </div>
+    <!-- end Scroll to Top -->
+
+    @include('header')
+
+    <!-- Hero section -->
+    <div class="section-2xl">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-9 order-lg-2 text-lg-end">
+                    <h1 class="display-4 fw-bold line-height-140 m-0">Шохрух Солеев — SEO-специалист из Ташкента</h1>
+                </div>
+                <div class="col-12 col-lg-3 order-lg-1">
+                    <a class="button-circle button-circle-2xl button-circle-dark button-circle-animation-drop lightbox-media-link"
+                       href="https://www.youtube.com/watch?v=W-j4QGAgNu8"><i class="fas fa-play"></i></a>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end Hero section -->
+
+    <!-- About section -->
+    <div class="section-xl pt-0">
+        <div class="container">
+            <div class="row align-items-center g-4 g-lg-5">
+                <div class="col-12 col-lg-3">
+                    <h2 class="fw-bold">Опыт <br>работы</h2>
+                    <a class="button-text-2 mt-4" href="#">Связаться со мной</a>
+                </div>
+                <div class="col-12 col-lg-9">
+                    <div class="bg-image parallax-bg" data-bg-src="../assets/images/agency-clean-2-parallax.jpg">
+                        <div class="bg-dark-05 p-5">
+                            <div class="row g-4">
+                                <div class="col-12 col-md-6">
+                                    <h5 class="fw-light m-0">Благодаря своему многолетнему опыту и комплексному подходу, помогаю бизнесу разного масштаба улучшить их онлайн-присутствие и достичь поставленных целей. </h5>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <p>В своей работе уделяю особое внимание пользовательскому опыту и интерфейсу. Разрабатываю интуитивно понятные и удобные интерфейсы, которые улучшают взаимодействие пользователей с продуктом и способствуют увеличению удовлетворенности клиентов.</p>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="progress-box">
+                                        <h6 class="fw-normal">SEO</h6>
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 90%;"
+                                                 aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                                                <span>90%</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="progress-box">
+                                        <h6 class="fw-normal">Разработка сайтов</h6>
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 75%;"
+                                                 aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                                                <span>75%</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="progress-box">
+                                        <h6 class="fw-normal">Реклама в гугл</h6>
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 70%;"
+                                                 aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                                                <span>70%</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="progress-box">
+                                        <h6 class="fw-normal">UI/UX</h6>
+                                        <div class="progress">
+                                            <div class="progress-bar" role="progressbar" style="width: 60%;"
+                                                 aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                                                <span>60%</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- end row(inner) -->
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end About section -->
+
+    <!-- Services section -->
+    <div class="section-xl pt-0">
+        <div class="container text-center">
+            <div class="mb-5 text-center">
+                <h6 class="fw-medium uppercase letter-spacing-2 text-dark-04">Что я делаю</h6>
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-4">
+                    <div class="f-box style-1">
+                        <div class="f-box-icon">
+                            <i class="ti-rocket"></i>
+                        </div>
+                        <h5 class="fw-normal">Продвигаю сайты в гугл</h5>
+                        <p>Хотите, чтобы ваш сайт был на первых позициях в Google? Я помогу вам этого добиться! С моим опытом и знаниями в области SEO, ваш сайт станет более заметным для целевой аудитории, что приведет к увеличению трафика и росту продаж.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="f-box style-1">
+                        <div class="f-box-icon">
+                            <i class="ti-star"></i>
+                        </div>
+                        <h5 class="fw-normal">Создаю сайты</h5>
+                        <p>Нужен современный и функциональный веб-сайт, который привлекает клиентов и соответствует вашим бизнес-целям? Я предлагаю услуги по созданию сайтов любой сложности — от простых лендингов до сложных корпоративных порталов.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <div class="f-box style-1">
+                        <div class="f-box-icon">
+                            <i class="ti-layers"></i>
+                        </div>
+                        <h5 class="fw-normal">Прототипирую</h5>
+                        <p>Прототипирование — ключевой этап создания успешного веб-продукта. Я разрабатываю интерактивные прототипы, которые позволяют увидеть и протестировать структуру и функциональность сайта или приложения ещё до начала разработки. </p>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end Services section -->
+
+    <!-- Portfolio section -->
+    <div class="section-xl pt-0">
+        <div class="container">
+            <div class="row align-items-center g-4 g-lg-5">
+                <div class="col-12 col-lg-4">
+                    <h2 class="fw-bold">Мои проекты</h2>
+                    <a class="button-text-2 mt-4" href="#">смотреть все</a>
+                </div>
+                <div class="col-12 col-lg-8">
+                    <div class="owl-carousel owl-nav-overlap-left hover-style-3" data-owl-items="1"
+                         data-owl-dots="false" data-owl-nav="true">
+                        <!-- 1 -->
+                        <div class="portfolio-item">
+                            <a href="/portfolio/single/portfolio-single-1.html">
+                                <div class="portfolio-img">
+                                    <img src="../assets/images/agency-creative-p1.jpg" alt="">
+                                </div>
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-box">
+                                        <h3 class="uppercase letter-spacing-2 m-0">Project Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- 2 -->
+                        <div class="portfolio-item">
+                            <a href="/portfolio/single/portfolio-single-1.html">
+                                <div class="portfolio-img">
+                                    <img src="../assets/images/agency-creative-p2.jpg" alt="">
+                                </div>
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-box">
+                                        <h3 class="uppercase letter-spacing-2 m-0">Project Title</h3>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end Portfolio section -->
+
+    <!-- Clients section -->
+    <div class="section-xl pt-0">
+        <div class="container">
+            <div class="mb-5 text-center">
+                <h6 class="fw-medium uppercase letter-spacing-2 text-dark-04">our amazing clients</h6>
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-10 offset-lg-1">
+                    <ul class="clients-grid column-4">
+                        <li><a href="#"><img src="../assets/images/client-1.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-2.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-3.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-4.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-5.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-6.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-7.png" alt=""></a></li>
+                        <li><a href="#"><img src="../assets/images/client-8.png" alt=""></a></li>
+                    </ul>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end Clients section -->
+
+    <!-- Footer -->
+    <div class="section bg-dark">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-12 col-lg-7">
+                    <h6 class="fw-medium mb-lg-3">Свяжитесь со мной!</h6>
+                    <p>Ищете надежного специалиста по SEO, веб-разработке, рекламе в Google или UI/UX дизайну? Я готов помочь вам улучшить ваше онлайн-присутствие и достичь ваших целей. Давайте обсудим ваш проект и найдем лучшее решение для вашего бизнеса.</p>
+                        <p>Напишите мне или позвоните, чтобы начать сотрудничество!</p>
+                    <ul class="list-inline-sm mt-3 mt-lg-4">
+                        <li><a class="button-circle button-circle-sm button-circle-white" href="#"><i
+                                    class="fab fa-facebook-f"></i></a></li>
+                        <li><a class="button-circle button-circle-sm button-circle-white" href="#"><i
+                                    class="fab fa-twitter"></i></a></li>
+                        <li><a class="button-circle button-circle-sm button-circle-white" href="#"><i
+                                    class="fab fa-pinterest"></i></a></li>
+                        <li><a class="button-circle button-circle-sm button-circle-white" href="#"><i
+                                    class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-lg-5">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="fw-medium mb-lg-3">Useful links</h6>
+                            <ul class="list-dash">
+                                <li><a href="#">Clients</a></li>
+                                <li><a href="#">Contact</a></li>
+                                <li><a href="#">Our Team</a></li>
+                                <li><a href="#">Services</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <h6 class="fw-medium mb-lg-3">Additional links</h6>
+                            <ul class="list-dash">
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">Prices</a></li>
+                                <li><a href="#">Process</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                            </ul>
+                        </div>
+                    </div><!-- end row(inner) -->
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+    <!-- end Footer -->
+
+</div><!-- end wrapper -->
+
+<!-- ***** JAVASCRIPTS ***** -->
+<script src="../assets/plugins/jquery.min.js"></script>
+<script src="../assets/plugins/plugins.js"></script>
+<script src="../assets/js/functions.min.js"></script>
+</body>
+</html>
